9fc7ead505e9846c7cf70694ffd55239915d8a26
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-03-04 10:16:29 +0200

    feat(products): implement POST /products to create a new product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,27 +3,48 @@ const mongoose = require('mongoose');
 
 // GET /api/products
 const getAllProducts = async (req, res) => {
-  res.send("getAllProducts");
+  res.send('getAllProducts');
 };
 
 // POST /api/products
 const createProduct = async (req, res) => {
-  res.send("createProduct");
+  const {
+    productName,
+    caregory,
+    description,
+    price,
+    inventoryCount,
+    supplier,
+  } = req.body;
+  try {
+    const product = await Proroduc.create({
+      productName,
+      caregory,
+      description,
+      price,
+      inventoryCount,
+      supplier,
+    });
+    return res.status(201).json(product);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  // res.send('createProduct');
 };
 
 // GET /api/products/:productId
 const getProductById = async (req, res) => {
-  res.send("getProductById");
+  res.send('getProductById');
 };
 
 // PUT /api/products/:productId
 const updateProduct = async (req, res) => {
-  res.send("updateProduct");
+  res.send('updateProduct');
 };
 
 // DELETE /api/products/:productId
 const deleteProduct = async (req, res) => {
-  res.send("deleteProduct");
+  res.send('deleteProduct');
 };
 
 module.exports = {
