6bb186532a1c2442f0cb4995874118ef1283ca4b
NipaDelara <nipadelara@hotmail.com>
2026-03-04 13:10:51 +0200

    feat(user):user model run crash fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    fullName: { type: String, require: true },
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true },
-    phoneNumber: { type: String, require: true },
-    gender: { type: String, require: true },
-    date_of_birth: { type: String, require: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    gender: { type: String, required: true },
+    date_of_birth: { type: String, required: true },
   },
-  { timestamps: true, versionkey: false },
+  { timestamps: true, versionKey: false },
 );
 
-model.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
